0e7026edb9d6bfb9e38781fce9dd02f0e650baf5
louisgab <louisgab@live.fr>
2018-03-03 13:23:20 +0100

    contact form is now inside page content with wpcf7 shortcode

--- web/app/themes/HTML-Theme/contact.php
+++ web/app/themes/HTML-Theme/contact.php
@@ -34,38 +34,7 @@ while ( have_posts() ) : the_post(); ?>
     <div class="three-fourth last">
         <h3>Let's keep in touch</h3>
         <?php the_content(); ?>
-                <form action="php/contact-send.php" method="post" class="contact-form">
-
-                    <p class="input-block">
-                        <label for="contact-name"><strong>Name</strong> (required)</label>
-                        <input type="text" name="name" value="" id="contact-name" required>
-                    </p>
-
-                    <p class="input-block">
-                        <label for="contact-email"><strong>Email</strong> (required)</label>
-                        <input type="email" name="email" value="" id="contact-email" required>
-                    </p>
-
-                    <p class="input-block">
-                        <label for="contact-subject"><strong>Subject</strong></label>
-                        <input type="text" name="subject" value="" id="contact-subject">
-                    </p>
-
-                    <p class="textarea-block">
-                        <label for="contact-message"><strong>Your Message</strong> (required)</label>
-                        <textarea name="message" id="contact-message" cols="88" rows="6" required></textarea>
-                    </p>
-
-                    <div class="hidden">
-                        <label for="contact-spam-check">Do not fill out this field:</label>
-                        <input name="spam-check" type="text" value="" id="contact-spam-check" />
-                    </div>
-
-                    <input type="submit" value="Submit">
-
-                    <div class="clear"></div>
-
-                </form>
+            
 
             </div><!-- end .three-fourth.last -->
 
