0c3c3212143cf387257829bb588ac33284af8af7
louisgab <louisgab@live.fr>
2018-03-05 00:51:06 +0100

    added acf to contact + i18n

--- web/app/themes/HTML-Theme/contact.php
+++ web/app/themes/HTML-Theme/contact.php
@@ -3,36 +3,45 @@
 Template Name: Contact
 */
 get_header();
-
-while ( have_posts() ) : the_post(); ?>
-
+while ( have_posts() ) : the_post();
+$info = get_field( 'info' );
+?>
 <div class="container clearfix">
     <header class="page-header">
-        <h1 class="page-title"><?php the_title(); ?></h1>
+        <h1 class="page-title">
+            <?php the_title(); ?>
+        </h1>
     </header>
 </div>
 
 <section id="map">
-    <p class="container">Something went wrong... Try to enable your JavaScript!</p>
+    <p class="container">
+        <?= __( 'Something went wrong... Try to enable your JavaScript!', THEME_DOMAIN ); ?>
+    </p>
 </section>
 
 <div class="container clearfix">
     <div class="one-fourth">
-        <h3>Contact Info</h3>
+        <h3>
+            <?= __( 'Contact Info', THEME_DOMAIN ); ?>
+        </h3>
         <p>
-            30 South Park Avenue<br/>
-            San Francisco, CA 94108<br/>
-            USA
+            <?= $info['adress']; ?>
         </p>
         <p>
-            Phone: (123) 456-7890<br/>
-            Fax: +08 (123) 456-7890<br/>
-            Email: contact@companyname.com<br/>
-            Web: companyname.com
+            <?= __( 'Phone', THEME_DOMAIN ) . ': ' . $info['phone']; ?>
+            <br/>
+            <?= __( 'Fax', THEME_DOMAIN ) . ': ' . $info['fax']; ?>
+            <br/>
+            <?= __( 'Email', THEME_DOMAIN ) . ': ' . $info['email']; ?>
+            <br/>
+            <?= __( 'Web', THEME_DOMAIN ) . ': ' . $info['website']; ?>
         </p>
     </div>
     <div class="three-fourth last">
-        <h3>Let's keep in touch</h3>
+        <h3>
+            <?= __( 'Let\'s keep in touch', THEME_DOMAIN ); ?>
+        </h3>
         <?php the_content(); ?>
     </div>
 </div>
